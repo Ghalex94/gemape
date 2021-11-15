32b80a8f001d3d3a63b4bc6dcac7c3740234c1cb
alexa <alexa@Lenovo>
2021-11-15 09:07:42 -0500

    Primer test

---
+++ src/TEST.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, " this is a test");
+
+	}
+
+}
