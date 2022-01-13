fba65b37bb8e9beb294d19c546df40f902105c11
alexa <alexa@Lenovo>
2022-01-13 12:27:04 -0500

    botoncillo

--- src/Home.java
+++ src/Home.java
@@ -4,10 +4,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Home extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -30,11 +32,14 @@ public class Home extends JFrame {
 	 */
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 619, 393);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(159, 142, 89, 23);
+		contentPane.add(btnNewButton);
 	}
-
 }
